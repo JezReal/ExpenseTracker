32a0bf05374b6f282de1d7d2edd122ab0b9dc859
jez <jezreeljoshuam@gmail.com>
2023-09-13 23:17:12 +0800

    :sparkles: add date in expense

--- expensetracker/models.py
+++ expensetracker/models.py
@@ -1,4 +1,6 @@
+import django.utils.timezone
 from django.db import models
+from django.utils.timezone import now
 
 
 # Create your models here.
@@ -7,3 +9,4 @@ class ExpenseItem(models.Model):
     expense_item_id = models.BigAutoField(primary_key=True)
     item_name = models.CharField(max_length=500)
     item_price = models.FloatField()
+    date = models.DateTimeField(default=django.utils.timezone.now)
